ba4317657cf54819f03f10243d3d8667e6e35e7f
Thea <theola51924@stud.noroff.no>
2024-02-10 22:33:59 +0700

    working on the correlation between routes-files and the main index-files

--- src/js/routes/home.js
+++ src/js/routes/home.js
@@ -1,5 +1,17 @@
-import { setAuthListener } from "../ui/listeners/auth.js";
+import * as listener from "../ui/listeners/index.js";
 
 export async function homePage() {
-  setAuthListener()
+  listener.authListener()
+  listener.postListener()
 }
+
+import * as templates from "./templates/index.js";
+import * as HTTPMethod from "./api/requests/index.js";
+
+async function testTemplate() {
+  const posts = await HTTPMethod.getPostsFromAPI();
+  const postContainer = document.querySelector(".post-example");
+  templates.renderPostTemplates(posts, postContainer);
+}
+
+testTemplate();
