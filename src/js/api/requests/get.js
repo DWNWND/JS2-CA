62b5a81060e9d8d15658d89e6c4bd22973ca5fbf
Thea <theola51924@stud.noroff.no>
2024-02-11 15:57:18 +0700

    added query params to get author, comments and reactions, also cleaned up old comments

--- src/js/api/requests/get.js
+++ src/js/api/requests/get.js
@@ -1,26 +1,14 @@
 import { fetchWithToken } from "../fetchWithToken.js";
 import { API_BASE, API_POSTS } from "../constants.js";
 
-// //can also be called "getPosts" - up to you. But remeber to change name in all files
-// export async function readPosts() {
-//   const response = await fetchWithToken(API_BASE + API_POSTS);
-//   console.log(await response.json);
-//   return await response.json();
-// }
-
-// //from Olis YT tutorial:
-// //can also be called "getPost" - up to you. But remeber to change name in all files
-// export async function readPost(id) {}
-
-//In the new API V2 its all nesten in .data property
-
 //extra queryParams for extra info in the request
 const authorParam = "?_author=true";
-const commentsParam = "?comments=true";
-const reactionsParam = "?reactions=true";
+const commentsParam = "&_comments=true";
+const reactionsParam = "&_reactions=true";
 
 export async function getPostsFromAPI() {
-  const getPostsURL = API_BASE + API_POSTS + authorParam;
+  // const getPostsURL = API_BASE + API_POSTS + authorParam;
+  const getPostsURL = `${API_BASE}${API_POSTS}?_author=true&_comments=true&_reactions=true`;
   const response = await fetchWithToken(getPostsURL);
 
   const posts = await response.json();
@@ -34,12 +22,12 @@ export async function getPostFromAPI(id) {
   if (!id) {
     throw new Error("getPost funciton is missing a postID");
   }
-  const getPostURL = `${API_BASE}${API_POSTS}/${id}${authorParam}`;
+  const getPostURL = `${API_BASE}${API_POSTS}/${id}?_author=true&_comments=true&_reactions=true`;
   const response = await fetchWithToken(getPostURL);
 
   const post = await response.json();
   const singlePost = post.data;
-  console.log("POST BY ID: ", singlePost);
+  // console.log("POST BY ID: ", singlePost);
 
   return singlePost;
 }
