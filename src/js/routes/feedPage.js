3f703bc40b980a72a12c313dd29ab0c18f9210a6
Thea <theola51924@stud.noroff.no>
2024-02-11 15:57:54 +0700

    adjusted the router file

---
+++ src/js/routes/feedPage.js
@@ -0,0 +1,23 @@
+import * as listener from "../ui/listeners/index.js";
+import * as templates from "../templates/index.js";
+import * as HTTPMethod from "../api/requests/index.js";
+
+async function buildFeed() {
+  const posts = await HTTPMethod.getPostsFromAPI();
+  const feedContainer = document.querySelector(".feed-container");
+  templates.renderPostTemplates(posts, feedContainer);
+}
+
+export async function feedPage() {
+  buildFeed();
+  // listener.postListener();
+}
+
+//RENDER POSTS
+// async function testTemplate() {
+//   const posts = await HTTPMethod.getPostsFromAPI();
+//   const postContainer = document.querySelector(".post-example");
+//   templates.renderPostTemplates(posts, postContainer);
+// }
+
+// testTemplate();
