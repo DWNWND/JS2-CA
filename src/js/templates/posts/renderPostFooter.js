c5388169ec068239237da0909b9cd062b7806049
Thea <theola51924@stud.noroff.no>
2024-02-12 20:14:44 +0700

    moved the files into a separate post folder

---
+++ src/js/templates/posts/renderPostFooter.js
@@ -0,0 +1,17 @@
+import { displayReactionsAccordion, displayCommentsAccordion } from "../index.js";
+
+export function renderPostFooter(postData) {
+  const accordionItemReactions = displayReactionsAccordion(postData);
+  const accordionItemComments = displayCommentsAccordion(postData);
+
+  const accordion = document.createElement("div");
+  accordion.classList.add("accordion");
+  accordion.id = "view-likes-and-comments";
+  accordion.append(accordionItemReactions, accordionItemComments);
+
+  const cardFooter = document.createElement("div");
+  cardFooter.classList.add("card-footer", "text-muted");
+  cardFooter.append(accordion);
+
+  return cardFooter;
+}
