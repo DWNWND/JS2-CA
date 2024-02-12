3b641c70b48ce82562a4737c532fc4c76e7d35be
Thea <theola51924@stud.noroff.no>
2024-02-12 20:25:18 +0700

    added a flex-grow-1 class to the accordion and removed the closebtn

--- src/js/templates/modals/renderModalFooter.js
+++ src/js/templates/modals/renderModalFooter.js
@@ -5,23 +5,23 @@ export function renderModalFooter(postData) {
   const accordionItemComments = displayCommentsAccordion(postData);
 
   const accordion = document.createElement("div");
-  accordion.classList.add("accordion");
+  accordion.classList.add("accordion", "flex-grow-1");
   accordion.id = "view-likes-and-comments";
   accordion.append(accordionItemReactions, accordionItemComments);
 
   const modalFooter = document.createElement("div");
   modalFooter.classList.add("modal-footer", "text-muted");
 
-  const closeBtn = document.createElement("div");
-  closeBtn.classList.add("btn", "btn-secondary")
-  closeBtn.setAttribute("data-bs-dismiss=", "modal");
-  closeBtn.innerText = "close modal"
+  // const closeBtn = document.createElement("div");
+  // closeBtn.classList.add("btn", "btn-secondary")
+  // closeBtn.setAttribute("data-bs-dismiss", "modal");
+  // closeBtn.innerText = "close modal"
 
   // const saveBtn = document.createElement("div");
   // saveBtn.classList.add("btn", "btn-primary")
   // saveBtn.innerText = "save changes"
-  
-  modalFooter.append(accordion, closeBtn);
+
+  modalFooter.append(accordion);
 
   return modalFooter;
 }
