e0e32a43e6525bc019c5ad5aaa363718e0d691c5
Thea <theola51924@stud.noroff.no>
2024-02-12 14:14:30 +0700

    removed comments

--- src/js/templates/renderPostFooter.js
+++ src/js/templates/renderPostFooter.js
@@ -15,12 +15,3 @@ export function cardFooter(postData) {
 
   return cardFooter;
 }
-
-
-  //COMMENTS
-  //   <label for="new-comment" class="d-none"></label>
-  //     <strong><h5 class="username m-0">Username</h5></strong>
-  //     <div class="input-group">
-  //       <textarea class="form-control" aria-label="Add comment" id="new-comment" placeholder="Type your comment here"></textarea>
-  //       <button class="btn btn-outline-secondary" type="submit" id="submit-new-comment">Share</button>
-  //     </div>
