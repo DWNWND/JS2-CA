25ceffd60432ead6a923baff4d8c780c4034e2b1
Thea <theola51924@stud.noroff.no>
2024-02-16 22:00:19 +0700

    working on the filtering functionality

--- src/js/ui/events/filter.js
+++ src/js/ui/events/filter.js
@@ -1,13 +1,22 @@
-import { renderPostTemplates } from "../../templates/posts/index.js";
+// import { renderPostTemplates } from "../../templates/posts/index.js";
 
-export function getPostsFromFiltering(postsFromAPI) {
-  let postsFiltered = postsFromAPI.filter((allPosts) => {
-    if (document.getElementById("sortbymostliked").checked && allPosts._count.reactions > 0) {
-      return allPosts;
-    }
-  });
-  const feedContainer = document.querySelector(".feed-container");
-  feedContainer.innerHTML = "";
+// export function getPostsFromFiltering(postsFromAPI) {
+//   let postsFiltered = postsFromAPI.filter((allPosts) => {
+//     if (document.getElementById("sortbymostliked").checked && allPosts._count.reactions > 0) {
+//       return allPosts;
+//     }
+//     if (document.getElementById("sortbytypethreads").checked && allPosts.title && !allPosts.body) {
+//       return allPosts;
+//     }
+//     if (document.getElementById("sortbytypephoto").checked && allPosts.media) {
+//       return allPosts;
+//     }
+//     if (document.getElementById("sortbytypephoto").checked && document.getElementById("sortbymostliked").checked) {
+//       console.log("working on this");
+//     }
+//   });
+//   const feedContainer = document.querySelector(".feed-container");
+//   feedContainer.innerHTML = "";
 
-  renderPostTemplates(postsFiltered, feedContainer);
-}
+//   renderPostTemplates(postsFiltered, feedContainer);
+// }
--- src/js/ui/listeners/onFilter.js
+++ src/js/ui/listeners/onFilter.js
@@ -1,16 +1,59 @@
 import { startFeed } from "../../routes/index.js";
-import { getPostsFromFiltering } from "../events/index.js";
+import { renderPostTemplates } from "../../templates/posts/index.js";
 import { openPostAsModal } from "./index.js";
 
-export function filter(postsFromAPI) {
-  const sortingOptions = document.getElementById("sortbymostliked");
+//Simplify this:
 
-  sortingOptions.addEventListener("change", async (event) => {
+const sortingByLikes = document.getElementById("sortbymostliked");
+const sortingByThreads = document.getElementById("sortbytypethreads");
+const sortingByPhotos = document.getElementById("sortbytypephoto");
+
+export function getPostsFromFiltering(postsFromAPI) {
+  let postsFiltered = postsFromAPI.filter((allPosts) => {
+    if (sortingByLikes.checked && allPosts._count.reactions > 0) {
+      return allPosts;
+    }
+    if (sortingByThreads.checked && allPosts.title && !allPosts.body) {
+      return allPosts;
+    }
+    if (sortingByPhotos.checked && allPosts.media) {
+      return allPosts;
+    }
+    if (sortingByPhotos.checked && sortingByLikes.checked && allPosts.media) {
+      console.log("working on this");
+    }
+  });
+  const feedContainer = document.querySelector(".feed-container");
+  feedContainer.innerHTML = "";
+
+  renderPostTemplates(postsFiltered, feedContainer);
+}
+
+function sorting(filter, postsFromAPI) {
+  filter.addEventListener("change", async (event) => {
     if (event.target.checked) {
-      getPostsFromFiltering(postsFromAPI)
+      getPostsFromFiltering(postsFromAPI);
       await openPostAsModal();
-    } else {
+    }
+    if (!event.target.checked && sortingByLikes.checked) {
+      getPostsFromFiltering(postsFromAPI);
+      await openPostAsModal();
+    }
+    if (!event.target.checked && sortingByThreads.checked) {
+      getPostsFromFiltering(postsFromAPI);
+      await openPostAsModal();
+    }
+    if (!event.target.checked && sortingByPhotos.checked) {
+      getPostsFromFiltering(postsFromAPI);
+      await openPostAsModal();
+    } else if (!event.target.checked && !sortingByPhotos.checked && !sortingByThreads.checked && !sortingByLikes.checked) {
       await startFeed(postsFromAPI);
     }
   });
+}
+
+export function filter(postsFromAPI) {
+  sorting(sortingByLikes, postsFromAPI);
+  sorting(sortingByThreads, postsFromAPI);
+  sorting(sortingByPhotos, postsFromAPI);
 }
