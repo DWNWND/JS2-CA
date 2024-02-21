89f56256442a64dbf9151cfc33c764016b4f5c0e
Thea <theola51924@stud.noroff.no>
2024-02-21 20:26:54 +0700

    added function to check if there is a token in local storage when loading feed html, and if not redirect to login page

--- src/js/routes/feedPage.js
+++ src/js/routes/feedPage.js
@@ -2,12 +2,13 @@ import * as listenFor from "../ui/listeners/index.js";
 import * as templates from "../templates/posts/index.js";
 import * as HTTPMethod from "../api/requests/index.js";
 import { makeModal } from "../templates/modals/index.js";
+import { load } from "../storage/index.js";
 
 export async function startFeed(allPosts) {
   const feedContainer = document.querySelector(".feed-container");
   feedContainer.innerHTML = "";
   templates.renderPostTemplates(allPosts, feedContainer);
-  listenFor.logOut()
+  listenFor.logOut();
   await listenFor.openPostAsModal();
 }
 
@@ -17,28 +18,34 @@ export async function feedPage() {
   try {
     const posts = await HTTPMethod.getPostsFromAPI();
 
-    let params = new URLSearchParams(document.location.search);
-    let postId = params.get("post-id");
-    let id = parseInt(postId);
-
-    posts.filter(async (allPosts) => {
-      if (allPosts.id === id) {
-        await makeModal(id);
-        let myModal = new bootstrap.Modal(document.getElementById(`modal-${id}`), {});
-        myModal.toggle();
-        listenFor.removeModals();
-      } else {
-        return;
-      }
-    });
-
     if (posts) {
+      let params = new URLSearchParams(document.location.search);
+      let postId = params.get("post-id");
+      let id = parseInt(postId);
+
+      posts.filter(async (allPosts) => {
+        if (allPosts.id === id) {
+          await makeModal(id);
+          let myModal = new bootstrap.Modal(document.getElementById(`modal-${id}`), {});
+          myModal.toggle();
+          listenFor.removeModals();
+        } else {
+          return;
+        }
+      });
       await startFeed(posts);
       listenFor.filter(posts);
       listenFor.search(posts);
       listenFor.publishNewPost();
     } else if (!posts) {
-      throw new Error("something went wrong when calling API");
+      const token = load("token");
+      if (!token) {
+        localStorage.clear();
+        location.pathname = "/";
+      }
+      if (token) {
+        throw new Error("something went wrong when calling API");
+      }
     }
   } catch (error) {
     console.log(error);
