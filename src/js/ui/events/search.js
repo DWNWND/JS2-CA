429512f1c685a746c0a2a09a3f3b97dce356475a
Thea <theola51924@stud.noroff.no>
2024-03-02 19:50:56 +0700

    fixed the search functionality to work with the pagedisplay and loadmore

--- src/js/ui/events/search.js
+++ src/js/ui/events/search.js
@@ -1,20 +1,24 @@
 import { renderPostTemplates } from "../../templates/posts/index.js";
+import { feedContainer } from "../../constants.js";
 
 const searchInput = document.querySelector("#searchbar");
 
-export function getPostsFromSearch(postsFromAPI) {
+export async function getPostsFromSearch(allPostsFromAPI, postsPerPage) {
   let query = searchInput.value;
 
-  let postsSearched = postsFromAPI.filter((allPosts) => {
-    if (query === "") {
-      return postsFromAPI;
-    } else if (allPosts.title.toLowerCase().includes(query.toLowerCase())) {
+  let postsSearched = allPostsFromAPI.filter((allPosts) => {
+    if (allPosts.title.toLowerCase().includes(query.toLowerCase())) {
+      return allPosts;
+    }
+    if (allPosts.body && allPosts.body.toLowerCase().includes(query.toLowerCase())) {
       return allPosts;
     }
   });
 
-  const feedContainer = document.querySelector(".feed-container");
-  feedContainer.innerHTML = "";
+  if (query === "") {
+    postsSearched = postsPerPage;
+  }
 
+  feedContainer.innerHTML = "";
   renderPostTemplates(postsSearched, feedContainer);
 }
--- src/js/ui/listeners/onSearch.js
+++ src/js/ui/listeners/onSearch.js
@@ -1,13 +1,21 @@
+import { getAllPostsFromAPI, getPostsFromAPI } from "../../api/requests/get.js";
 import { masonry } from "../../ux/layout/index.js";
 import { getPostsFromSearch } from "../events/index.js";
 import { openPostAsModal } from "./index.js";
+import { loadMoreBtn } from "../../constants.js";
 
-export async function search(postsFromAPI) {
+let page = 1;
+
+export async function search() {
   const searchInput = document.querySelector("#searchbar");
+  const postsPerPage = await getPostsFromAPI(page);
 
   searchInput.addEventListener("input", async () => {
-    getPostsFromSearch(postsFromAPI);
+    loadMoreBtn.style.display = "none";
+    const allPosts = await getAllPostsFromAPI();
+
+    getPostsFromSearch(allPosts, postsPerPage);
     await openPostAsModal();
-    masonry;
+    masonry();
   });
 }
