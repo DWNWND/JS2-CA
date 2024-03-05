13250617c94ac939600c9556eaf9a0270fdd9afd
Thea <theola51924@stud.noroff.no>
2024-03-05 13:19:10 +0700

    edited the filtering names and cleaned up the code, api clls and modals

--- src/js/ui/events/filter.js
+++ src/js/ui/events/filter.js
@@ -1,71 +1,67 @@
-import { renderPostTemplate, renderPostTemplates } from "../../templates/posts/index.js";
-import { sortingByLikes, sortingByComments, sortingByThreads, sortingByPhotos, feedContainer, loadMoreBtn, loader } from "../../constants.js";
-import { getPostsFromAPI } from "../../api/requests/get.js";
+import { renderPostTemplates } from "../../templates/posts/index.js";
+import { postsByPage, allPosts, filterByLikes, filterByThreads, filterByPhotos, filterByComments, feedContainer, loadMoreBtn, loader } from "../../constants.js";
 import { openPostAsModal } from "../listeners/openModal.js";
 
-let page = 1;
-
-export async function filtering(allPosts) {
+export async function filterPosts() {
   loader.style.display = "flex";
 
-  //UPDATE TO STARTFEED
-  if (!sortingByComments.checked && !sortingByLikes.checked && !sortingByThreads.checked && !sortingByPhotos.checked) {
-    const postsByPage = await getPostsFromAPI(page);
+  if (!filterByComments.checked && !filterByLikes.checked && !filterByThreads.checked && !filterByPhotos.checked) {
     renderPostTemplates(postsByPage, feedContainer);
     loadMoreBtn.style.display = "block";
-    await openPostAsModal();
+    
   } else {
     loadMoreBtn.style.display = "none";
 
-    if (sortingByLikes.checked) {
+    if (filterByLikes.checked) {
       const mostLiked = allPosts.filter(({ _count: { reactions, comments }, media, title }) => {
-        if (!sortingByPhotos.checked && !sortingByThreads.checked && !sortingByComments.checked && reactions > 2) {
+        if (!filterByPhotos.checked && !filterByThreads.checked && !filterByComments.checked && reactions > 2) {
           return allPosts;
         }
-        if (!sortingByPhotos.checked && !sortingByThreads.checked && sortingByComments.checked && reactions > 2 && comments > 2) {
+        if (!filterByPhotos.checked && !filterByThreads.checked && filterByComments.checked && reactions > 2 && comments > 2) {
           return allPosts;
         }
-        if (sortingByPhotos.checked && !sortingByThreads.checked && !sortingByComments.checked && reactions > 2 && media) {
+        if (filterByPhotos.checked && !filterByThreads.checked && !filterByComments.checked && reactions > 2 && media) {
           return allPosts;
         }
-        if (!sortingByPhotos.checked && sortingByThreads.checked && !sortingByComments.checked && reactions >= 2 && title && !media) {
+        if (!filterByPhotos.checked && filterByThreads.checked && !filterByComments.checked && reactions >= 2 && title && !media) {
           return allPosts;
         }
       });
       renderPostTemplates(mostLiked, feedContainer);
     }
 
-    if (sortingByComments.checked) {
+    if (filterByComments.checked) {
       const mostCommented = allPosts.filter(({ _count: { comments }, media, title }) => {
-        if (!sortingByLikes.checked && !sortingByThreads.checked && !sortingByPhotos.checked && comments > 2) {
+        if (!filterByLikes.checked && !filterByThreads.checked && !filterByPhotos.checked && comments > 2) {
           return allPosts;
         }
-        if (!sortingByLikes.checked && sortingByThreads.checked && !sortingByPhotos.checked && comments > 2 && title && !media) {
+        if (!filterByLikes.checked && filterByThreads.checked && !filterByPhotos.checked && comments > 2 && title && !media) {
           return allPosts;
         }
-        if (!sortingByLikes.checked && !sortingByThreads.checked && sortingByPhotos.checked && comments > 2 && media) {
+        if (!filterByLikes.checked && !filterByThreads.checked && filterByPhotos.checked && comments > 2 && media) {
           return allPosts;
         }
       });
       renderPostTemplates(mostCommented, feedContainer);
     }
 
-    if (sortingByThreads.checked) {
+    if (filterByThreads.checked) {
       const sortThreads = allPosts.filter(({ media, title }) => {
-        if (!sortingByComments.checked && !sortingByLikes.checked && !sortingByPhotos.checked && title && !media) {
+        if (!filterByComments.checked && !filterByLikes.checked && !filterByPhotos.checked && title && !media) {
           return allPosts;
         }
       });
       renderPostTemplates(sortThreads, feedContainer);
     }
 
-    if (sortingByPhotos.checked) {
+    if (filterByPhotos.checked) {
       const sortPhotos = allPosts.filter(({ media }) => {
-        if (!sortingByComments.checked && !sortingByLikes.checked && !sortingByThreads.checked && media) {
+        if (!filterByComments.checked && !filterByLikes.checked && !filterByThreads.checked && media) {
           return allPosts;
         }
       });
       renderPostTemplates(sortPhotos, feedContainer);
     }
   }
+  await openPostAsModal();
 }
