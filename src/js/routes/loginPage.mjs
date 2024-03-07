0882337d5f7bcdf461d7700d620779fb368c2db7
Thea <theola51924@stud.noroff.no>
2024-03-07 11:56:26 +0700

    bugfixing

--- src/js/routes/loginPage.mjs
+++ src/js/routes/loginPage.mjs
@@ -1,6 +1,8 @@
 import * as listenFor from "../ui/listeners/index.mjs";
+import { generalErrorContainer } from "../constants.mjs";
 
 export function loginPage() {
   listenFor.authentication();
   listenFor.validation();
+  listenFor.clearErrorMessages(generalErrorContainer);
 }
