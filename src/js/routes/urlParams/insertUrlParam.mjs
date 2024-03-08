258b2894fef7e4d1d5ac87db2e078d684258b90a
Thea <theola51924@stud.noroff.no>
2024-03-08 14:11:52 +0700

    generalised the param functions

--- src/js/routes/urlParams/insertUrlParam.mjs
+++ src/js/routes/urlParams/insertUrlParam.mjs
@@ -1,15 +1,15 @@
 /**
- * Adds a new searchParam calles "post-id" to the url and updates the url with the new url without refreshing.
+ * Adds a new searchParam to the url and updates the url with the new url without refreshing.
+ * 
  * Inspired by:
- * https://stackoverflow.com/questions/10970078/modifying-a-query-string-without-reloading-the-page
  * https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams/set
  *
  * @param {string, number} id Passes in the id of the post that you want added to the searchParam.
  */
-export function insertUrlParam(id) {
+export function insertUrlParam(searchParam, id) {
   if (history.pushState) {
     let searchParams = new URLSearchParams(window.location.search);
-    searchParams.set("post-id", id);
+    searchParams.set(searchParam, id);
     let newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + "?" + searchParams.toString();
     window.history.pushState({ path: newurl }, "", newurl);
   }
